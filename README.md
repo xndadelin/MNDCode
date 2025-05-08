1e2bd7301b9fd1458b95c07fafb9b0576103c718
ValyMnD <mindrilavasilevalentin@gmail.com>
2025-05-08 17:01:19 +0300

    BIG_UPDATE

---
+++ README.md
@@ -0,0 +1,24 @@
+# MNDCode
+**Learn Programming the Smart Way!**  
+
+## 🚀 Introduction  
+**MNDCode** is an interactive platform designed to teach programming concepts `step by step`. Whether you're a beginner or looking to polish your skills, **MNDCode** provides engaging lessons and practical exercises.
+
+## ⚙️ Installation  
+**Clone the repo**  
+```bash
+git clone https://github.com/ValyMnDul/MNDCode.git
+cd MNDCode
+```
+**Install the dependencies**
+```bash
+sudo npm install
+```
+**Run the app**
+```bash
+node app.js
+```
+
+## 📞 Contact
+- EMAIL: mindrilavasilevalentin@gmail.com
+- STEAM: https://steamcommunity.com/id/valymnd/
---
+++ app.js
@@ -0,0 +1,44 @@
+const express=require('express');
+const app=express();
+
+app.set('view engine','ejs');
+
+app.listen(3000,'0.0.0.0');
+console.log("Listening for requests on port 3000...");
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>
+{
+    res.render('home',{title:"TheName"});
+});
+
+app.get('/compiler',(req,res)=>
+{
+    res.render('compiler',{title:"Compiler"});
+});
+
+app.get('/lectii',(req,res)=>
+{
+    res.render('lectii',{title:"Lectii"});
+});
+
+app.get('/lectii/1',(req,res)=>
+{
+    res.render('lectii_1',{title:"Lectia 1"});
+});
+
+app.get('/lectii/2',(req,res)=>
+{
+    res.render('lectii_2',{title:"Lectia 2"});
+});
+
+app.get('/lectii/3',(req,res)=>
+{
+    res.render('lectii_3',{title:"Lectia 3"});
+});
+
+app.use((req,res)=>
+{
+    res.status(404).render('404',{title:"404"});
+});
